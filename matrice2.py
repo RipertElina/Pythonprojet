83cb0cf965cea71754417ec27a4e9d24657aaf7e
RipertElina <ripert.elina@gmail.com>
2016-04-04 14:47:40 +0200

    test mt traceback

---
+++ matrice2.py
@@ -0,0 +1,181 @@
+'''
+TO DO 
+
+Faire une matrice de traceback, creer une matrice de talle seq1 et seq2 puis
+quand on calcul le score max, on recupere la val de max genre si c'est diag on dis que c'est D à la position de i,j
+Puis on remonde la traceback pour afficher l'alignement 
+Si diag : deux lettre l'une en face de l'autre, même si elles sont differentes
+Si left : on met un gap sur la seq verticale (la 2) 
+Si right : idem mais avec la seq1
+Et du coup on peux parcoucrir dans le sens qu'on veux pour ecrire l'allignement ;) 
+
+
+Voir aussi pour les seq qui ne font pas la même taille 
+
+'''
+import numpy
+
+
+class Matrice:
+  #Definition de la classe matrice avec une matrice, deux sequences, les scores de match, mismatch et gap
+ def __init__(self):
+  self.__mt = []
+  self.__tbMt = []
+  self.__s1 = ''
+  self.__s2 = ''
+  self.__match = 4
+  self.__mismatch = -4
+  self.__gap = -4
+  self.__score = None 
+
+
+ #getter
+ def getMatch(self) : 
+  return self.__match
+ def getGap(self) : 
+  return self.__gap
+ def getMiss(self) : 
+  return self.__mismatch
+ def getScore(self) : 
+  return self.__score
+
+ #setter 
+ def setMatch(self,x) : 
+  self.__match = int(x)
+ def setGap(self,x) : 
+  self.__gap = int(x)  
+ def setMiss(self,x) : 
+  self.__mismatch = int(x)
+ def setS1(self,s) : 
+ 	self.__s1 = s
+ def setS2(self,s) : 
+ 	self.__s2 = s
+ def setScore(self,score) : 
+  self.__score = score
+
+ #initialise la matrice (m,n) a zero
+ def initZero(self, shape):
+  self.__mt = []
+  #Ligne
+  for x in range(shape[0]):               
+   self.__mt.append([])
+  #colonne
+  for y in range(shape[1]):
+   for z in range(shape[1]):
+    self.__mt[-1-y].append(0)
+  return self.__mt
+
+ def initZeroTB(self, shape):
+  self.__tbMt = []
+  #Ligne
+  for x in range(shape[0]):
+   self.__tbMt.append([])
+  #colonne
+  for y in range(shape[1]):
+   for z in range(shape[1]):
+    self.__tbMt[-1-y].append(0)
+  return self.__tbMt
+ 
+ #Renvoie le score si deux entitées sont egales (match), sinon gap, sinon mismatch 
+ def match_score(self, x, y):
+  if x == y:
+   return self.__match
+  elif x == '-' or y == '-':
+   return self.__gap
+  else:
+   return self.__mismatch
+  
+ #retourne la sequence 1 et 2 dans le bon sens
+ def finalize(self, align1, align2):
+  align1 = align1[::-1] 
+  align2 = align2[::-1] 
+  i,j = 0,0
+  
+  #calcul de l'identite, score et rend l'alignement de sequence 
+  symbol = ''
+  score = 0
+  identity = 0
+  outFileRes = open("outFileRes.txt","w")
+
+  for i in range(0,len(align1)):
+   #Si deux AA sont les meme, on aligne 
+   if align1[i] == align2[i]:
+    symbol = symbol + '*'
+    identity = identity + 1
+    
+   #Si il y a un mismatch 
+   elif align1[i] != align2[i] and align1[i] != '-' and align2[i] != '-':
+    symbol += '!'
+    
+   #Si gap, sortie d'un espace
+   elif align1[i] == '-' or align2[i] == '-':
+    symbol += '-'
+        
+   identity = round(float(identity) / len(align1) * 100, 2)
+  
+  outFileRes.write("Score          : "+str(self.getScore())+"\n")
+  outFileRes.write("Identite       : "+str(identity)+"%\n")
+  outFileRes.write("Sequence 1     : "+str(align1)+"\n")
+  outFileRes.write("Sequence 2     : "+str(align2)+"\n")
+  outFileRes.write("Correspondance : "+str(symbol)+"\n\n")
+
+  outFileRes.close()
+    
+  #Rempli la matrice avec les bon scores 
+ def needle(self):
+  m = len(self.__s1)
+  n = len(self.__s2)
+  
+  #Genere la matrice par programmation dynamique et le chemin de traceback
+  scoreMt = self.initZero((m+1, n+1))
+  traceback = self.initZeroTB((m+1,n+1))
+  #Calcul de la matrice
+  traceback[0][0] = "Done"
+  #colonnes
+  for i in range(0, m + 1):
+   scoreMt[i][0] = self.__gap * i
+   traceback[i+1][0] = "L" 
+  #Lignes
+  for j in range(0, n + 1):
+   scoreMt[0][j] = self.__gap * j
+   traceback[j+1][0] = "U"
+  for i in range(1, m + 1):
+   for j in range(1, n + 1):
+    diag = scoreMt[i-1][j-1] + self.match_score(self.__s1[i-1], self.__s2[j-1])
+    up = scoreMt[i-1][j] + self.__gap
+    left = scoreMt[i][j-1] + self.__gap
+    val = max(diag, up, left)
+    scoreMt[i][j] =  val
+    if val == diag : 
+    	traceback[i][j] = 'D'
+    elif val == up : 
+    	traceback[i][j] = "U"
+    elif val == left : 
+    	traceback[i][j] = "L"
+
+  self.setScore(score[i][j])
+
+  #Taceback et calcul l'alignement
+  align1 = ''
+  align2 = ''
+  i = m
+  j = n
+  while traceback[i][j] != "Done" : 
+
+   if traceback[i][j] == "D" :  
+    align1 += self.__s1[i-1]
+    align2 += self.__s2[j-1]	
+    i -= 1
+    j -= 1
+
+   elif traceback[i][j] == "U" : 
+    align1 += '-'
+    align2 += self.__2[j-1]
+    j -= 1
+
+   elif traceback[i][j] == "L" : 
+   	align1 += self.__s1[i-1]
+    align2 += '-'
+    i -= 1
+   
+  self.finalize(align1, align2)
---
+++ menu2.py
@@ -0,0 +1,136 @@
+from matrice import Matrice
+matrice = Matrice()
+
+def menu() : 
+
+ print("Bonjour, bienvenue dans l'outil d'alignement needle\n Menu principal : choississez une rubrique\n")
+ print("1. Visualiser les scores\n")
+ print("2. Changer les scores\n")
+ print("3. Alignement avec deux fichiers de sequence \n")
+ print("4. Alignement à partir de deux sequences saisit manuellement\n")
+ print("5. Lire l'alignement depuis un fichier de sortie\n")
+ print("6. Quitter\n")
+ choix = input( "Entrez votre choix (1 a 6)  : \n")
+ if choix == "1" : 
+  affScore()
+  print("Retour au menu")
+  menu()
+ elif choix == "2" : 
+  changeScore()
+ elif choix == "3" : 
+  AliFile()
+ elif choix == "4" : 
+  AliManu()
+ elif choix == "5" : 
+  outFile()
+ elif choix == "6" : 
+  exit(0)
+ else : 
+  print("Votre choix n'existe pas, veuillez saisir un nombre en 1 a 6 : \n")
+  menu()
+
+#Fonction permettant d'afficher les scores actuelles 
+def affScore() : 
+ print("Score de match : "+str(matrice.getMatch())+"\n")
+ print("Score de gap : "+str(matrice.getGap())+"\n")
+ print("Score de missmatch : "+str(matrice.getMiss())+"\n")
+
+#Fonction permettant de changer des scores
+def changeScore() : 
+ match = input("Score de match : \n")
+
+ try :
+  int(match)
+ except : 
+  print("Merci de saisir une valeur correcte")
+  changeScore()
+
+ gap = input("Score de gap : \n")
+
+ try :
+  int(gap)
+ except : 
+  print("Merci de saisir une valeur correcte")
+  changeScore()
+
+ mismatch = input("Score de mismatch : \n")
+
+ try :
+  int(mismatch)
+ except : 
+  print("Merci de saisir une valeur correcte")
+  changeScore()
+
+ matrice.setMatch(match)
+ matrice.setGap(gap)
+ matrice.setMiss(mismatch)
+ return
+
+#Allignement depuis des sequence prise dans des fichiers externe 
+def AliFile() : 
+ seq1File = None
+ seq2File = None
+
+ print("Bienvenue dans le mode d'alignement de sequence à partir de fichiers d'entres \n")
+
+ fichier1 = input("Entrez le nom du fichier comportant la sequence 1 : (Defaut : seq1.txt) \n")
+ if(fichier1 == "") :
+  fichier1 = "seq1.txt"
+
+ try : 
+  seq1File = open(fichier1, "r")
+ except : 
+  print("Le fichier 1 ", fichier1, "est introuvable")
+  AliFile()
+
+ fichier2 = input("Entrez le nom du fichier comportant la sequence 2 : (Defaut : seq2.txt) \n")
+ if(fichier2 == "") :
+  fichier2 = "seq2.txt"
+
+ try : 
+  seq2File = open(fichier2, "r")
+ except : 
+  print("Le fichier 2 ", fichier2, "est introuvable")
+  AliFile()
+ 
+ # On lit les fichiers
+ seq1 = seq1File.read()
+ seq2 = seq2File.read()
+
+ # On set les sequences correspondantes
+ matrice.setS1(seq1)
+ matrice.setS2(seq2)
+
+ print("Lancement de l'alignement \n")
+ matrice.needle()
+
+ # On ferme les fichiers en ecriture
+ seq1File.close()
+ seq2File.close()
+ return
+
+#Alignement depuis des sequences saisit manuellement
+def AliManu() : 
+ print("Bienvenue dans le mode d'alignement de sequence à partir de la saisie manuelle de vos sequences \n")
+ seq1 = input("Saisir la premier sequence \n")
+ seq2 = input("Saisir la deuxieme sequence \n")
+ matrice.setS1(seq1)
+ matrice.setS2(seq2)
+ print("Lancement de l'alignement \n")
+ matrice.needle()
+ return
+
+#Lis le fichier de sorti
+def outFile() :
+ try : 
+  outFileR = open("FichierSortiAlignement.txt", "r")
+ except : 
+  print("Le fichier de sortie est inreouvable, veillez faire un alignement avant de le consulter")
+  menu()
+ for ligne in outFileR : 
+  print(ligne+"\n")
+ # On ferme les fichiers
+ outFileR.close()
+
+while True:
+ menu()
